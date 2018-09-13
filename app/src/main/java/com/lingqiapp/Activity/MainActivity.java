23ff93eeac87c14c240a188cd5365b4e32680e8b
sakura <975976959@qq.com>
2018-09-13 09:42:40 +0800

    Initial commit

---
+++ app/src/main/java/com/lingqiapp/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lingqiapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.lingqiapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
